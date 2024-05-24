abc3eed2db5d71d971463beb4c5e65093d211f86
Music <Music@DESKTOP-USM63RM>
2024-05-24 20:48:08 +0530

    commit

---
+++ BasicsJava/src/Day3JavaProgram/ForLoop.java
@@ -0,0 +1,13 @@
+package Day3JavaProgram;
+
+public class ForLoop 
+		{
+	public static void main(String[] args) 
+		{
+		for (int i = 1; i<=1000; i++)        //(int i = 1000; i<=1; i--)
+		{
+			System.out.println("Value of i :");
+			System.out.println(i);
+		}
+	}
+}
