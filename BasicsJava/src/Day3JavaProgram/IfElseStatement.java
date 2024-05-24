0d96ee756861c4bfc2b11b97463b7b02f6c9fa19
Music <Music@DESKTOP-USM63RM>
2024-05-24 09:37:00 +0530

    This Describes a Java program

---
+++ BasicsJava/src/Day3JavaProgram/IfElseStatement.java
@@ -0,0 +1,15 @@
+package Day3JavaProgram;
+
+public class IfElseStatement {
+
+	public static void main(String[] args) 
+	{
+	int age = 11;
+	if(age>=18)
+		System.out.println("You are eligible for Voting");
+	else
+		System.out.println("Sorry! you need to wait more");
+		System.out.println("if else demonstration");
+	}
+
+}
