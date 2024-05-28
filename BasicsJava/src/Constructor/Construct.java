05995132c725cec6e185a9e5111729a3af936a96
Music <Music@DESKTOP-USM63RM>
2024-05-28 11:13:14 +0530

    This Describes a Java Program

---
+++ BasicsJava/src/Constructor/Construct.java
@@ -0,0 +1,60 @@
+package Constructor;
+
+public class Construct 
+	{
+		private String name;
+		private int id;
+		private int roll;
+		
+		public Construct()
+		{
+			super();
+			//System.out.println("Default Constructor");
+			//name = "Nova";
+			//id = 24319;
+			//roll = 22;
+		}
+			public Construct (String name, int id)
+			{
+				super();
+			System.out.println("Parameterized Constructor with 2 arguments : ");
+			this.name = name;
+			this.id = id;
+			}
+		    public Construct (String name, int id, int roll)
+		    {
+			System.out.println("Parameterized Constructor with 3 arguments :  ");
+			this.name = name;
+			this.id = id;
+			this.roll = roll;
+		    }
+			public String getName()
+			{
+				return name;
+			}
+			public void SetName (String name)
+			{
+				this.name = name;
+			}
+			public int getId()
+			{
+				return id;
+			}
+			public void setId (int id)
+			{
+				this.id = id;
+			}
+			public int getroll()
+			{
+				return roll;
+			}
+			public void setroll (int roll)
+			{
+				this.roll = roll;
+			}
+			@Override
+			public String toString() 
+			{
+				return "Construct [name=" + name + ", id=" + id + ", roll=" + roll + "]";
+			}
+	}
---
+++ BasicsJava/src/Constructor/ConstructDemo.java
@@ -0,0 +1,19 @@
+package Constructor;
+
+public class ConstructDemo
+		{
+	public static void main(String[] args)
+		{
+		//Object for default Constructor:
+		Construct c = new Construct ();
+		System.out.println(c);
+		
+		//Object for parameterized Constructor:
+		Construct c1 = new Construct ("Nova",22);
+		System.out.println(c1);
+		
+		//Object for Overloaded Constructor:
+		Construct c2 = new Construct ("Nova",22,23);
+		System.out.println(c2);
+	}
+}
